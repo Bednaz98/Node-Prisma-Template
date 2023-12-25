4f3413c98195741aa6c68ca40142a67eb3f1bf68
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2023-12-24 21:17:35 -0600

    add docker support

---
+++ src/prisma.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+
+export function getDBStringDebug(envString: string | undefined) {
+    if (!envString) return "N/A";
+    const firstSplit = envString.split('://')
+    const dbType = firstSplit[0]
+    const ipAddress = firstSplit[1].split(':')[1].split("@")[1]
+    const port = firstSplit[1].split(':')[2].split("/")[0]
+    return (`\n- db type: ${dbType}\n- host ip: ${ipAddress}:${port}\n- white check: ${envString.trim() === envString}`);
+}
+
+
+const getPrisma = (): PrismaClient => {
+    console.log("\ngenerating prisma client #########################\n");
+    try {
+        const dbString = process?.env?.["LOG_DATABASE_URL"];
+        if (dbString) {
+            return new PrismaClient({
+                datasources: {
+                    db: {
+                        url: dbString.trim(),
+                    },
+                },
+            })
+        }
+        console.log('direct db string injection failed, using default prisma client\n');
+        return new PrismaClient()
+    } catch (error) {
+        console.log("prisma client error, using default client")
+        console.error(error)
+        return new PrismaClient()
+    }
+}
+
+export const prisma = getPrisma();
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import express, { Response, Request } from "express"
+/**
+ * This is a debug server for testing, 
+ * down below you can edit the config to test the logger settings
+*/
+import express from "express"
 import { defaultFunction } from "./routes/routes";
+import { getDBStringDebug } from "./prisma";
 
 
 
 
 
-
+console.log('server initiating ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 const app = express();
 app.use(express.json());
 app.disable('x-powered-by');
@@ -18,9 +23,25 @@ app.put("*", defaultFunction);
 app.patch("*", defaultFunction);
 app.delete("*", defaultFunction);
 
-const httpPort = process.env?.["SERVER_PORT"] ?? 3000;
-app.listen(httpPort, () => { console.log(`Starting test server: ${httpPort}`) });
+function getPort() {
+    const loggerPort = Number(process?.env["LOGGER_PORT"])
+    if (!isNaN(loggerPort)) {
+        console.log("detected logger port: ", loggerPort)
+        return loggerPort;
+    }
+    const serverPort = Number(process.env?.["SERVER_PORT"])
+    if (!isNaN(serverPort)) {
+        console.log("detected sever port: ", serverPort)
+        return Number(serverPort)
+    }
+    console.log("using default port: ", 3000);
+    return 3000;
+}
 
+const port = getPort();
+console.log(getDBStringDebug(process.env?.["LOG_DATABASE_URL"]));
+const server = app.listen(port, () => { console.log(`Starting test server: ${port}`) });
+export default server;
 
 
 
