5e428873eeeff974e1b295883622fcfe32f32467
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2024-04-29 09:45:14 -0500

    Update jabPrisma.ts

--- jabPrisma.ts
+++ jabPrisma.ts
@@ -1,45 +1,29 @@
 import fs from 'fs-extra'
 
 const jabDir = './node_modules/@jabz/'
-
-const defaultRemove = [`generator client {\nprovider = "prisma-client-js"\n}`, `datasource db {\nprovider = "postgresql"\nurl      = env("DATABASE_URL")\n}`, `// This is your Prisma schema file,
-// learn more about it in the docs: https://pris.ly/d/prisma-schema`];
-
-
-const appendHeader = `generator client {
-    provider = "prisma-client-js"
-}
-
-datasource db {
-    provider = "postgresql"
-    url      = env("DATABASE_URL")
-}`
-
-
-const editPrismaPath = './prisma/editSchema.prisma'
-const defaultPrismaPath = './prisma/schema.prisma'
+const defaultPrismaPath = './prisma/'
+const prismaConst = "/prisma"
 
 async function getPrismaFiles() {
-    const directories: string[] = await fs.readdir(jabDir).catch(() => []).then((e) => e)
-    return Promise.all(directories.map(async (m) => {
+    const nodeModules: string[] = await fs.readdir(jabDir).catch(() => []).then((e) => e)
+    return (await Promise.all(nodeModules.map(async (m) => {
         try {
-            const fileData = (await fs.readFile(jabDir + m + '/prisma/editSchema.prisma')).toString();
-            return fileData
+            const prismaFiles: string[] = (await fs.readdir(jabDir + m + prismaConst).catch(() => []).then((e) => e)) ?? [];
+            if (prismaFiles.length < 1) return [];
+            const promiseFiles = await Promise.all(prismaFiles.map(async e => ({ fileName: e, file: (await fs.readFile(jabDir + m + e)).toString() })));
+            return promiseFiles;
         } catch (error) {
-            return '';
+            console.error(error)
+            return [];
         }
-    }));
-
+    }))).reduce((p, c) => ([...p, ...c]), []);
 }
 
 (async () => {
     try {
         const rawFiles = await getPrismaFiles();
-        const editFiles = rawFiles;
-        const editScheme = (await fs.readFile(editPrismaPath).catch(e => { console.error(e); return "" })).toString();
-        const fileArray = [appendHeader, editScheme, ...editFiles].map((e) => e.replace(defaultRemove[0], '').replace(defaultRemove[1], '').replace(defaultRemove[2], '').trim());
-        const newFile = fileArray.filter((e) => e.length > 0).join('\n\n');
-        await fs.writeFile(defaultPrismaPath, newFile);
+        const workFunction = rawFiles.map(async e => { fs.writeFile(defaultPrismaPath + e.fileName, e.file) })
+        await Promise.all(workFunction);
     } catch (error) {
         console.error(error);
     }
